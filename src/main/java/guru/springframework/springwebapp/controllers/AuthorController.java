b636da37ecf447264b883ce9237c776871ee0de8
evandro <ebolzan@inf.ufsm.br>
2021-08-05 23:07:25 -0300

    List authors and show in the browser

---
+++ src/main/java/guru/springframework/springwebapp/controllers/AuthorController.java
@@ -0,0 +1,27 @@
+package guru.springframework.springwebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.springwebapp.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+	
+	private AuthorRepository authorRepository;
+	
+	public AuthorController(AuthorRepository authorRepository) {
+		this.authorRepository = authorRepository;
+	}
+	
+	@RequestMapping("/authors")
+	public String getAuthors(Model model)
+	{
+		model.addAttribute("authors", authorRepository.findAll());
+		
+		return "author/list2";
+		
+	}
+
+}
